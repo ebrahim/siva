a73bedd8fceeaf42789f5d13109d282aed0b09e2
Mohammad Ebrahim Mohammadi Panah <ebrahim@mohammadi.ir>
2010-07-10 17:08:29 +0430

    * Fixed major bug by reverting to rails 2.3.5 * Improved word edit form

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,13 +4,24 @@ class Word < ActiveRecord::Base
 	has_many :synonymies, :foreign_key => :word1_id
 	has_many :synonymies, :foreign_key => :word2_id
 
+	before_save do |word|
+		word.word_forms.sort! { |a,b| a.text <=> b.text }
+		word.word_forms = word.word_forms.inject([]) do |result, item|
+			result << item if not result.last or (not item.text.blank? and result.last.text != item.text)
+			result
+		end
+		if word.word_forms.blank?
+			word.errors.add :word_forms, I18n.t(:'activerecord.errors.models.word.attributes.word_forms.too_short')
+			false
+		end
+	end
+
 	after_destroy do |word|
 		Synonymy.destroy_all :word1_id => word.id
 		Synonymy.destroy_all :word2_id => word.id
 	end
 
 	validates_presence_of :language
-	#validates_size_of :word_forms, :minimum => 1		# FIXME: Disables destoying word_forms
 
 	accepts_nested_attributes_for :word_forms, :allow_destroy => true, :reject_if => lambda { |params| params[:text].blank? }
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,5 +8,5 @@ function add_fields(link, association, content)
 {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).up().insert({ before: content.replace(regexp, new_id) });
+  $(link).insert({ before: content.replace(regexp, new_id) });
 }
