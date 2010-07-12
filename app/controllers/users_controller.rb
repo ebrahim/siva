4809b94fd4c4b0533f7e40d6ee7d2dfa2fe4ab5d
Mohammad Ebrahim Mohammadi Panah <ebrahim@mohammadi.ir>
2010-07-13 04:11:32 +0430

    * Added a fantastic user management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,74 +1,108 @@
 class UsersController < ApplicationController
-  skip_before_filter :verify_authenticity_token, :only => :create
-  
-  def new
-    @user = User.new
-  end
- 
-  def create
-    logout_keeping_session!
-    if using_open_id?
-      authenticate_with_open_id(params[:openid_url], :return_to => open_id_create_url, 
-        :required => [:nickname, :email]) do |result, identity_url, registration|
-        if result.successful?
-          create_new_user(:identity_url => identity_url, :login => registration['nickname'], :email => registration['email'])
-        else
-          failed_creation(result.message || t(:user_something_wrong))
-        end
-      end
-    else
-      create_new_user(params[:user])
-    end
-  end
-  
-  def activate
-    logout_keeping_session!
-    user = User.find_by_activation_code(params[:activation_code]) unless params[:activation_code].blank?
-    case
-    when (!params[:activation_code].blank?) && user && !user.active?
-      user.activate!
-      flash[:notice] = t(:user_singup_complete)
-      redirect_to login_path
-    when params[:activation_code].blank?
-      flash[:error] = t(:user_activation_missing)
-      redirect_back_or_default(root_path)
-    else 
-      flash[:error]  = t(:user_activation_invalid)
-      redirect_back_or_default(root_path)
-    end
-  end
-  
-  protected
-  
-  def create_new_user(attributes)
-    @user = User.new(attributes)
-    if @user && @user.valid?
-      if @user.not_using_openid?
-        @user.register!
-      else
-        @user.register_openid!
-      end
-    end
+	skip_before_filter :verify_authenticity_token, :only => :create
+	require_role :admin, :only => [ :index ]
 
-	@user.roles << Role.find_by_name('editor')
+	def index
+		@users = User.paginate :page => params[:page]
+	end
+
+	def new
+		@user = User.new
+	end
+ 
+	def create
+		logout_keeping_session!
+		if using_open_id?
+			authenticate_with_open_id(params[:openid_url], :return_to => open_id_create_url,
+				:required => [:nickname, :email]) do |result, identity_url, registration|
+				if result.successful?
+					create_new_user(:identity_url => identity_url, :login => registration['nickname'], :email => registration['email'])
+				else
+					failed_creation(result.message || t(:user_something_wrong))
+				end
+			end
+		else
+			create_new_user(params[:user])
+		end
+	end
+
+	def show
+		@user = User.find params[:id]
+		redirect_to :logout unless current_user && @user && (current_user.id == @user.id || current_user.has_role?('admin'))
+	end
+
+	def edit
+		@user = User.find params[:id]
+		redirect_to :logout unless current_user && @user && (current_user.id == @user.id || current_user.has_role?('admin'))
+	end
+
+	def update
+		@user = User.find params[:id]
+
+		if @user.update_attributes params[:user]
+			flash[:notice] = t(:user_password_updated)
+			redirect_to @user
+		else
+			flash[:error] = @user.errors.full_messages
+			redirect_to :action => :edit
+		end
+	end
+
+	def destroy
+		@user = User.find params[:id]
+		@user.destroy
+		flash[:notice] = t :user_removed
+		redirect_to :action => :index
+	end
+
+	def activate
+		logout_keeping_session!
+		user = User.find_by_activation_code(params[:activation_code]) unless params[:activation_code].blank?
+		case
+		when (!params[:activation_code].blank?) && user && !user.active?
+			user.activate!
+			flash[:notice] = t(:user_singup_complete)
+			redirect_to login_path
+		when params[:activation_code].blank?
+			flash[:error] = t(:user_activation_missing)
+			redirect_back_or_default(root_path)
+		else
+			flash[:error]  = t(:user_activation_invalid)
+			redirect_back_or_default(root_path)
+		end
+	end
+  
+	protected
+  
+	def create_new_user(attributes)
+		@user = User.new(attributes)
+		if @user && @user.valid?
+			if @user.not_using_openid?
+				@user.register!
+			else
+				@user.register_openid!
+			end
+		end
+
+		@user.roles << Role.find_by_name('editor')
     
-    if @user.errors.empty?
-      successful_creation(@user)
-    else
-      failed_creation
-    end
-  end
+		if @user.errors.empty?
+			successful_creation(@user)
+		else
+			failed_creation
+		end
+	end
   
-  def successful_creation(user)
-    redirect_back_or_default(root_path)
-    flash[:notice] = t(:user_signup_thanks)
-    flash[:notice] << " " + t(:user_sending_activation) if @user.not_using_openid?
-    flash[:notice] << " " + t(:user_openid_enabled) unless @user.not_using_openid?
-  end
+	def successful_creation(user)
+		redirect_back_or_default(root_path)
+		flash[:notice] = t(:user_signup_thanks)
+		flash[:notice] << " " + t(:user_sending_activation) if @user.not_using_openid?
+		flash[:notice] << " " + t(:user_openid_enabled) unless @user.not_using_openid?
+	end
   
-  def failed_creation(message = t(:user_signup_error))
-	@user = User.new if @user.nil?
-    flash[:error] = message
-    render :action => :new
-  end
+	def failed_creation(message = t(:user_signup_error))
+		@user = User.new if @user.nil?
+		flash[:error] = message
+		render :action => :new
+	end
 end
