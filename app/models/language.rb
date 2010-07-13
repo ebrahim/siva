e768710168c822a2c6babcaaed9335788682fad0
Mohammad Ebrahim Mohammadi Panah <ebrahim@mohammadi.ir>
2010-07-13 12:55:58 +0430

    * Improved language validations * Localized language's name field using localized_record * Upgraded and improved edit forms for categories and languages * Stronger title color

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,8 +1,12 @@
 class Language < ActiveRecord::Base
 	has_many :words, :dependent => :destroy
 
+	has_localized :name
+
 	validates_presence_of :name
 	validates_presence_of :code
 	validates_uniqueness_of :name
 	validates_uniqueness_of :code
+	validates_size_of :name, :within => 2..32
+	validates_size_of :code, :within => 2..3
 end
