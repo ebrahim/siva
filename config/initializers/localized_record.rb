f90034c1f649f4d294ca4a1d73eeb3a2812843fb
Mohammad Ebrahim Mohammadi Panah <ebrahim@mohammadi.ir>
2010-07-14 17:30:55 +0430

    * Fixed a bug in localized_record initializer * Added a migration to insert default languages into database * Improved config/database.yml templates

--- config/initializers/localized_record.rb
+++ config/initializers/localized_record.rb
@@ -1,2 +1,7 @@
 LocalizedRecord.mode = :tsv
-LocalizedRecord.available_locales = Hash[Language.find(:all, :select => 'code, name').map { |l| [l.code, l.name] }]
+begin
+	raise Exception.new if Language.all.blank?
+	LocalizedRecord.available_locales = Hash[Language.find(:all, :select => 'code, name').map { |l| [l.code, l.name] }]
+rescue Exception
+	LocalizedRecord.available_locales = { 'fa' => 'فارسی', 'en' => 'English' }
+end
