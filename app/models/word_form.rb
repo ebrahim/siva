c8f73d981d8d779db5d528bc0576a09e0555592f
Mohammad Ebrahim Mohammadi Panah <ebrahim@mohammadi.ir>
2010-07-18 04:14:06 +0430

    * Fixed it again

--- app/models/word_form.rb
+++ app/models/word_form.rb
@@ -1,8 +1,6 @@
 class WordForm < ActiveRecord::Base
 	belongs_to :word
 
-	default_scope :order => 'text ASC'
-
 	validates_presence_of :text
 	validates_uniqueness_of :text, :scope => :word_id
 	validates_length_of :text, :within => 2..128
