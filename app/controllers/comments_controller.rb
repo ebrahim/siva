52577d35a6e4ea66fad6e039b70fce135c36912e
Mohammad Ebrahim Mohammadi Panah <ebrahim@mohammadi.ir>
2010-07-18 04:59:24 +0430

    * Let the admin edit those synonymy comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
+	require_role :admin, :except => [ :create ]
 	require_role :editor
-	require_role :admin, :only => [ :destroy ]
 
 	def create
 		@comment = Comment.create params[:comment]
@@ -13,6 +13,21 @@ class CommentsController < ApplicationController
 			flash[:error] = @comment.errors.full_messages
 		end
 		redirect_to @synonymy
+	end
+
+	def edit
+		@comment = Comment.find params[:id]
+	end
+
+	def update
+		@comment = Comment.find params[:id]
+		if @comment.update_attributes params[:comment]
+			flash[:notice] = t :comment_updated
+			redirect_to @comment.commentable
+		else
+			flash[:error] = @comment.errors.full_messages
+			redirect_to :action => :edit
+		end
 	end
 
 	def destroy
